6e5289ae991db85b652101340291dd9e6011238b
Jing Qiang <tohjingqiang@gmail.com>
2021-07-22 19:30:07 +0800

    ics date, time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "./index.css";
 import { ICalendar } from 'datebook';
 import './ICal.js';
-const startdate = document.querySelector('#start-date');
+
+const startDateInput = document.querySelector('#start-date');
 const generateButton = document.querySelector('#generate');
 const textarea = document.querySelector('#textarea');
 
+const DAYS = {'MON': 0, 'TUE': 1, 'WED': 2, 'THU': 3, 'FRI': 4, 'SAT': 5, 'SUN': 6}
+
 // when generate timetable button is clicked
 generateButton.onclick = () => {
-    console.log(textarea.value);
     let textareaData = textarea.value;
     
     // return ics styled data
@@ -24,17 +26,39 @@ generateButton.onclick = () => {
 function generateData(textareaData) {
     // array of courses
     let courses = textareaData.split('\n');
+
+    // 1st course
+    let course = courses[0].split('\t');
+
+    // calculate date of lesson
+    let date = startDateInput.value;
+    // change the day
+    let day = parseInt(date.slice(8, 10));
+    day += DAYS[course[11]];
+    day = ("0" + day).slice(-2);
+    let actualDate = date.slice(0, 8).concat(day);
+
+    // get time duration
+    let time = course[12].split('-');
+
     const icalendar = new ICalendar({
-        title: 'Happy Hour',
-        location: 'The Bar, New York, NY',
-        description: 'Let\'s blow off some steam from our weekly deployments to enjoy a tall cold one!',
-        start: new Date('2020-07-04T19:00:00'),
-        end: new Date('2020-07-04T23:30:00'),
+        // Course: Title
+        title: `${course[0]}: ${course[1]}`,
+        // Venue
+        location: `${course[13]}`,
+        // Class_Type [Group]
+        description: `${course[9]} [${course[10]}]`,
+        start: new Date(`${actualDate}T${Math.floor(time[0] / 100)}:${time[0] % 100}:00`),
+        end: new Date(`${actualDate}T${Math.floor(time[1] / 100)}:${time[1] % 100}:00`),
         recurrence: {
             frequency: 'WEEKLY',
-            interval: 2
+            interval: 1,
+            count: 13
         }
     })
-    console.log(icalendar.render())
+    console.log(icalendar.render());
+    icalendar.download();
 }
 
+// Course	Title	AU 	Course Type 	Course Group	S/U Grade option	Index Number	Status  	Choice 	Class Type 	Group	 Day	  Time	  Venue	 Remark
+//AB1202	STATISTICS & ANALYSIS	3	CORE	 	 	00561	REGISTERED	 	SEM	13	THU	1030-1220	S4-SR17	Teaching Wk1-13
